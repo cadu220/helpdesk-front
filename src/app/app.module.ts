32b3bda60600c8ba3cfe96fee792cd8e3b85559a
duduarena <caduarena@gmail.com>
2022-11-22 13:55:56 -0300

    Html e css para header (criação component home)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { NavComponent } from './components/nav/nav.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
 // Componentes do projeto
 
@@ -35,6 +37,8 @@ import { NavComponent } from './components/nav/nav.component';
   declarations: [
     AppComponent,
     NavComponent,
+    HeaderComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
