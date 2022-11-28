3fd90938eb94601e1c4d0e62d901eaeca35ef5fb
duduarena <caduarena@gmail.com>
2022-11-28 12:33:09 -0300

    autentificação de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { NavComponent } from './components/nav/nav.component';
-import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { AuthGuard } from './auth/auth.guard'
+import { HomeComponent } from './components/home/home.component'
+import { LoginComponent } from './components/login/login.component'
+import { NavComponent } from './components/nav/nav.component'
+
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component'
 
 const routes: Routes = [
-  {
-    path:'login',
-    component:LoginComponent
-  },
+  { path: 'login', component: LoginComponent },
   {
     path: '',
-    component: NavComponent, children:[
-      {path: 'home', component: HomeComponent},
-      {path: 'tecnicos', component: TecnicoListComponent}
-
+    component: NavComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'tecnicos', component: TecnicoListComponent },
     ]
   }
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,34 +1,44 @@
-import { Component, OnInit } from '@angular/core'
-import { FormControl, Validators } from '@angular/forms'
-import { ToastrService } from 'ngx-toastr'
-import { Credenciais } from 'src/app/models/credenciais'
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Credenciais } from 'src/app/models/credenciais';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+
   creds: Credenciais = {
     email: '',
-    senha: '',
+    senha: ''
   }
-  email = new FormControl(null, Validators.email)
-  senha = new FormControl(null, Validators.minLength(3))
-  constructor(private toast: ToastrService) {}
 
-  ngOnInit(): void {}
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(3));
+
+  constructor(
+    private toast: ToastrService,
+    private service: AuthService,
+    private router: Router) { }
+
+  ngOnInit(): void { }
 
   logar() {
-    this.toast.error('Usuario ou senha invalidos', 'login')
-    this.creds.senha = ''
+    this.service.authenticate(this.creds).subscribe(resposta => {
+      this.service.successfulLogin(resposta.headers.get('Authorization').substring(7));
+      this.router.navigate([''])
+    }, () => {
+      this.toast.error('Usuário e/ou senha inválidos');
+      this.creds.senha = '';
+    })
   }
 
   validaCampos(): boolean {
-    if (this.email.valid && this.senha.valid) {
-      return true
-    } else {
-      return false
-    }
+    return this.email.valid && this.senha.valid
   }
+
 }
