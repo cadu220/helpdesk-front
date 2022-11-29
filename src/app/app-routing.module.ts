3c4e71751b59b4f5eb6478d5c018f2efbcb892f2
duduarena <caduarena@gmail.com>
2022-11-29 13:58:44 -0300

    rota para component tecnico-create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AuthGuard } from './auth/auth.guard'
 import { HomeComponent } from './components/home/home.component'
 import { LoginComponent } from './components/login/login.component'
 import { NavComponent } from './components/nav/nav.component'
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component'
 
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component'
 
@@ -16,6 +17,7 @@ const routes: Routes = [
     children: [
       { path: 'home', component: HomeComponent },
       { path: 'tecnicos', component: TecnicoListComponent },
+      { path: 'tecnicos/create', component: TecnicoCreateComponent },
     ]
   }
 ]
